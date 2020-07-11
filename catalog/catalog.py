8f5c621abdd01e6f88b194eac34a38e6f2ab19e9
Victor Fleurant <victorfleurant@gmail.com>
2020-07-11 14:54:22 -0400

    [FIX] Jaeger + add new traces

--- catalog/catalog.py
+++ catalog/catalog.py
@@ -5,6 +5,7 @@ import pymysql
 import opentracing
 from opentracing.ext import tags
 from jaeger_client import Config
+from opentracing.propagation import Format
 from opentracing.scope_managers.contextvars import ContextVarsScopeManager
 from logic import _retain_only_items_in_stock
 from db import _fetch_all_items, _create_new_item, _init_db
@@ -35,41 +36,52 @@ async def notify_server_started(app, loop):
 
 @app.route("/item", methods={"POST"})
 async def add_item(request):
-    name = request.json['name']
-    price = float(request.json['price'])
-    try:
-        _create_new_item(db, name, price)
-        return response.json(status=200, body="OK")
-    except Exception as e:
-        print(e)
-        return response.json(status=500, body=e)
+    tracer = opentracing.global_tracer()
+    with tracer.start_active_span('add_item') as scope:
+        name = request.json['name']
+        price = float(request.json['price'])
+        try:
+            _create_new_item(db, name, price)
+            return response.json(status=200, body="OK")
+        except Exception as e:
+            print(e)
+            return response.json(status=500, body=e)
 
 
 @app.route("/list")
 async def get_items(request):
-    try:
-        items = _fetch_all_items(db)
-        return response.json(status=200, body=items)
-    except Exception as e:
-        print(e)
-        return response.json(status=500, body=e)
+    tracer = opentracing.global_tracer()
+    with tracer.start_active_span('get_items') as scope:
+        try:
+            items = _fetch_all_items(db)
+            return response.json(status=200, body=items)
+        except Exception as e:
+            print(e)
+            return response.json(status=500, body=e)
 
 
 @app.route("/available")
 async def get_available_items(request):
-    try:
-        items = _fetch_all_items(db)
-        inventory = await _fetch_all_inventory()
-        available_items = _retain_only_items_in_stock(items, inventory)
-        return response.json(status=200, body=available_items)
-    except Exception as e:
-        print(e)
-        return response.json(status=500, body=e)
+    tracer = opentracing.global_tracer()
+    with tracer.start_active_span('get_available_items') as scope:
+        try:
+            items = _fetch_all_items(db)
+            inventory = await _fetch_all_inventory()
+            available_items = _retain_only_items_in_stock(items, inventory)
+            return response.json(status=200, body=available_items)
+        except Exception as e:
+            print(e)
+            return response.json(status=500, body=e)
 
 
 async def _fetch_all_inventory():
-    response = requests.get("http://inventory:8001/stocks")
-    return response.json()
+    headers = {}
+    tracer = opentracing.global_tracer()
+    with tracer.start_span("_fetch_all_inventory", child_of=tracer.active_span) as span:
+        span.set_tag(tags.HTTP_METHOD, "GET")
+        tracer.inject(span, Format.HTTP_HEADERS, carrier=headers)
+        response = requests.get("http://inventory:8001/stocks")
+        return response.json()
 
 
 if __name__ == "__main__":
